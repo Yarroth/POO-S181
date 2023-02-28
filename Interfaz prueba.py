32126b78dbebedad0016efc719309747467ac3ba
Yarroth <125915310+Yarroth@users.noreply.github.com>
2023-02-27 18:54:14 -0600

    interfaces TK Frame, botones

---
+++ Interfaz prueba.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# Función para mostrar el mensaje de confirmación
+def confirmacion(seccion):
+    respuesta = messagebox.askyesno("Confirmación", f"¿Seguro que desea continuar en la sección {seccion}?")
+    if respuesta:
+        print(f"Continuando en la sección {seccion}")
+    else:
+        print("Cancelado")
+
+# Crear la ventana
+ventana = tk.Tk()
+ventana.geometry("300x200")
+
+# Crear las tres secciones con diferentes colores
+seccion1 = tk.Frame(ventana, bg="red", width=100, height=200)
+seccion1.pack(side="top", fill="both", expand=True)
+
+seccion2 = tk.Frame(ventana, bg="green", width=100, height=200)
+seccion2.pack(side="top", fill="both", expand=True)
+
+seccion3 = tk.Frame(ventana, bg="blue", width=100, height=200)
+seccion3.pack(side="top", fill="both", expand=True)
+
+# Agregar un botón en cada sección para mostrar el mensaje de confirmación
+boton1 = tk.Button(seccion1, text="Rojo", bg="pink", command=lambda: confirmacion("1"))
+boton1.pack(pady=10, padx=40, fill="x")
+
+boton2 = tk.Button(seccion2, text="Verde", bg="yellow", command=lambda: confirmacion("2"))
+boton2.pack(pady=10, padx=40, fill="x")
+
+boton3 = tk.Button(seccion3, text="Azul", bg="orange", command=lambda: confirmacion("3"))
+boton3.pack(pady=10, padx=40, fill="x")
+
+# Mostrar la ventana
+ventana.mainloop()
